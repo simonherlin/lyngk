7c327d043b0f2dd070384865365ba2b382dca97b
sherlin <herlin.simon@gmail.com>
2017-09-29 15:46:09 +0200

    function is_valid

--- test/test.js
+++ test/test.js
@@ -3,7 +3,9 @@
 var LyngkTestCase = TestCase("LyngkTestCase");
 
 LyngkTestCase.prototype.testStory1 = function(){
-    var coordinates = new lyngk.Coordinates('A',1);
+    var coordinates = new Lyngk.Coordinates('A',1);
 
     assertFalse(coordinates.is_valid());
+
+
 };
