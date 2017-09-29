280737a240ee0e79ac8d5298762839f00b9cd728
sherlin <herlin.simon@gmail.com>
2017-09-29 14:18:01 +0200

    création test histoire un

--- test/test.js
+++ test/test.js
@@ -1,3 +1,9 @@
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+LyngkTestCase.prototype.testStory1 = function(){
+    var coordinates = new lyngk.Coordinates('A',1);
+
+    assertFalse(coordinates.is_valid());
+};s
