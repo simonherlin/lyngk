7c5e256902b2cf7eb53e446ffa5788b5b6394ad9
sherlin <herlin.simon@gmail.com>
2017-09-29 15:48:43 +0200

    création test histoire 2

--- test/test.js
+++ test/test.js
@@ -7,6 +7,6 @@ LyngkTestCase.prototype.testStory1 = function(){
 
     assertFalse(coordinates.is_valid());
 
-    assertEquals(coordinates.number_valid(),43);
+    assertEquals(coordinates.number_valid(),43); 
 
 };
