8e242e20654135ec2883a69a9dc053d75c6ba639
sherlin <herlin.simon@gmail.com>
2017-10-01 10:03:40 +0200

    correction histoire 7, chaque test histoire séparé

--- test/test.js
+++ test/test.js
@@ -2,32 +2,47 @@
 
 var LyngkTestCase = TestCase("LyngkTestCase");
 
-LyngkTestCase.prototype.testStory1 = function(){
-    var coordinates = new Lyngk.Coordinates('A',1);
 
-    // histoire 1
+// histoire 1
+LyngkTestCase.prototype.testStory1 = function() {
+    var coordinates = new Lyngk.Coordinates('A', 1);
     assertFalse(coordinates.is_valid());
+};
 
-    //histoire 2
-    assertEquals(coordinates.number_valid(),43);
+//histoire 2
+LyngkTestCase.prototype.testStory2 = function() {
+    var coordinates = new Lyngk.Coordinates('A', 1);
+    assertEquals(coordinates.number_valid(), 43);
+};
 
-    //histoire 3
-    coordinates.setLigne(3);
+//histoire 3
+LyngkTestCase.prototype.testStory3 = function() {
+    var coordinates = new Lyngk.Coordinates('A', 3);
     assertEquals(coordinates.representation(),"A3");
+};
 
-    //histoire 4
-    coordinates.setLigne(1);
-    assertEquals(coordinates.representation(),"invalid");
+//histoire 4
+LyngkTestCase.prototype.testStory2 = function() {
+    var coordinates = new Lyngk.Coordinates('A', 1);
+    assertEquals(coordinates.representation(), "invalid");
+};
 
-    //histoire 5
+//histoire 5
+LyngkTestCase.prototype.testStory2 = function() {
+    var coordinates = new Lyngk.Coordinates('A', 1);
     var coordinates2 = coordinates.clone();
     assertTrue(coordinates.representation() === coordinates2.representation());
+};
 
-    //histoire 6
-    coordinates.setLigne(3);
+//histoire 6
+LyngkTestCase.prototype.testStory2 = function() {
+    var coordinates = new Lyngk.Coordinates('A', 3);
     assertTrue(coordinates.hash() === 2);
+};
 
-    // hsitoire 7
-    assertTrue(coordinates.get_intersection() === "VACANT");
-
+// hsitoire 7
+LyngkTestCase.prototype.testStory2 = function() {
+    var coordinates = new Lyngk.Coordinates('A', 3);
+    var intersection = new Lyngk.Intersection(coordinates);
+    assertTrue(intersection.getState() === Lyngk.State.VACANT);
 };
