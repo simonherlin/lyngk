cfd0aaa07840a314f499ce231b115fa924a8dac9
sherlin <herlin.simon@gmail.com>
2017-10-15 14:02:35 +0200

    Modification histoire 7

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,10 +2,13 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c) {
+Lyngk.Intersection = function (/*c*/) {
+    var state = Lyngk.State.VACANT;
+
     this.getState = function() {
-        if (c.is_valid()){
-                return Lyngk.State.VACANT;
-        }
+        //if (c.is_valid()){
+                return state;
+        //}
     };
+
 };
