5a7ca938932f3822f17b76a9ac7441dbb30e4f74
sherlin <herlin.simon@gmail.com>
2017-10-20 10:44:20 +0200

    Création histoire 17

--- src/Engine.js
+++ src/Engine.js
@@ -15,6 +15,28 @@ Lyngk.Engine = function () {
     };
     Init();
 
+    function testPosition(debut, fin){
+        if (debut.is_valid() && fin.is_valid()) // test que les positions sont correct
+            if ((plateau[fin.hash()].getState() != Lyngk.State.VACANT)&& (plateau[debut.hash()].getState() != Lyngk.State.VACANT)) // test qu'il y a bien des pieces
+                if ((Math.abs(fin.hash() - debut.hash())) != 0) // verifie que l'on met pas a la meme place
+                    return true;
+        return false;
+    };
+
+    function testLigne(debut,fin){
+        var resultat = Math.abs(fin-debut);
+        if (resultat<10){ // verification que se soit que les decimal qui change
+            return true
+        }
+        if ((fin - Math.floor(fin/10)*10) == (debut - Math.floor(debut/10)*10)){ // verification que se soit que les dizaines qui change
+            return true;
+        }
+        if ((Math.floor(resultat/10)) == (resultat - Math.floor(resultat/10)*10)){ // verification diagonal
+            return true;
+        }
+        return false;
+    }
+
     this.initialisationUnePiece = function() {
         for (var key in plateau) {
             if (plateau.hasOwnProperty(key)) {
@@ -51,11 +73,16 @@ Lyngk.Engine = function () {
     };
 
     this.move = function(base, fin){
-        var hauteur =  plateau[base.hash()].getHauteur();
+        if (!(testPosition(base, fin)) || !(testLigne(base.hash(), fin.hash())))// test position
+            return false;
+        if (plateau[fin.hash()].getState() === Lyngk.State.VACANT)// test ligne diagonal
+            return false;
+        var hauteur =  plateau[base.hash()].getHauteur(); // obliger pour garder la meme hauteur car hauteur change quand on retire des pieces
         for (var i=0;i<hauteur;i++) {
             plateau[fin.hash()].pose(plateau[base.hash()].getFirstColor());
             plateau[base.hash()].remove();
         }
+        return true;
     };
 
     this.getPlat = function(){
